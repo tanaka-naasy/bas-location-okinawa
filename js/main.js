f8bb269bb0531f032be89cef4cb28cb460eb2619
tanaka-naasy <ekidendaisuki.746@gmail.com>
2025-12-30 19:48:04 +0000

    error For confirmation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-import { selectDisplayBuses } from "./busdata.js";
-import { renderBusList } from "./render.js";
-
 async function init() {
   try {
+    console.log("init start");
+
     const res = await fetch("data/sample_bus.json");
     const json = await res.json();
+    console.log("json loaded", json);
 
     const displayBuses = selectDisplayBuses(json.buses);
+    console.log("selected buses", displayBuses);
+
     renderBusList(displayBuses);
+    console.log("render called");
+
   } catch (error) {
     console.error("初期化エラー:", error);
   }
 }
-
-init();
