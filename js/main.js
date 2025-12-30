8118141d59b9667c0e8538bbcf2bc7f7d726f296
tanaka-naasy <ekidendaisuki.746@gmail.com>
2025-12-30 18:29:37 +0000

    Add buslogic and busdata for b-2phase

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 import { getBusData } from "./busdata.js";
 import { renderBusList } from "./render.js";
+import { selectDisplayBuses } from "./busdata.js";
+
+fetch("data/sample_bus.json")
+  .then(res => res.json())
+  .then(data => {
+    const displayBuses = selectDisplayBuses(data.buses);
+    renderBuses(displayBuses);
+  });
 
 async function init() {
   const busData = await getBusData();
